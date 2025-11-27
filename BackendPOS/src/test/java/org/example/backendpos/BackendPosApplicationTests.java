713eec45b255c58e437babdc412af17019f438be
nial0003 <nial0003@stud.kea.dk>
2025-11-27 08:40:16 +0100

    second commit

---
+++ BackendPOS/src/test/java/org/example/backendpos/BackendPosApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.backendpos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendPosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
