2f42604dcde508f18b19f03863e77663d960e8b4
nial0003 <nial0003@stud.kea.dk>
2025-12-03 12:58:20 +0100

    Created the Order model class. An order must contain a list of foods, a list of drinks, server name, table number, amount of guests, and a boolean to check if it has been sent

--- BackendPOS/src/main/java/org/example/backendpos/model/Order.java
+++ BackendPOS/src/main/java/org/example/backendpos/model/Order.java
@@ -1,4 +1,30 @@
 package org.example.backendpos.model;
 
+import jakarta.persistence.ManyToMany;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class Order {
+
+    private List<FoodItem> foodItems;
+    private List<DrinkItem> drinkItems;
+    private String nameOfServer;
+    private int tableNumber;
+    private int amountOfGuests;
+    private boolean hasBeenSent;
+
+    public Order(List<FoodItem> foodItems, List<DrinkItem> drinkItems, String nameOfServer, int tableNumber, int amountOfGuests, boolean hasBeenSent) {
+        this.foodItems = foodItems;
+        this.drinkItems = drinkItems;
+        this.nameOfServer = nameOfServer;
+        this.tableNumber = tableNumber;
+        this.amountOfGuests = amountOfGuests;
+        this.hasBeenSent = hasBeenSent;
+    }
+
+
 }
