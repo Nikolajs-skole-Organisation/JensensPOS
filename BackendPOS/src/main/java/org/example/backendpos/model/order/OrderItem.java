76953542c4829a8aea745ab73fd3d22429ac04c5
nial0003 <nial0003@stud.kea.dk>
2025-12-08 11:57:47 +0100

    added an orderItem class which will be the item added to the list. There is an incrementQuantity option so that you can increment if there's more than one

--- BackendPOS/src/main/java/org/example/backendpos/model/order/OrderItem.java
+++ BackendPOS/src/main/java/org/example/backendpos/model/order/OrderItem.java
@@ -1,5 +1,37 @@
 package org.example.backendpos.model.order;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Entity
+@Table(name = "order_item")
+@Getter
+@Setter
+@NoArgsConstructor
 public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id", nullable = false)
+    private Order order;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "food_item_id")
+    private FoodItem foodItem;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "drink_item_id")
+    private DrinkItem drinkItem;
+
+    @Column(nullable = false)
+    int quantity = 1;
+
+    public void incrementQuantity(){
+        quantity++;
+    }
 }
