a2a6816c4291fa61ba14b0a5e309790f767c1ef4
Daniel-Platz <dani0005@stud.kea.dk>
2025-12-11 15:07:38 +0100

    Updated repositories

--- BackendPOS/src/main/java/org/example/backendpos/repository/RestaurantTableRepository.java
+++ BackendPOS/src/main/java/org/example/backendpos/repository/RestaurantTableRepository.java
@@ -3,5 +3,10 @@ package org.example.backendpos.repository;
 import org.example.backendpos.model.RestaurantTable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RestaurantTableRepository extends JpaRepository<RestaurantTable, Long> {
+
+    Optional<RestaurantTable> findByTableNumber(int tableNumber);
+
 }
